a600a5ee1fe16df179a64e7b713024389c43ec85
Fantasy <1362724990@qq.com>
2025-12-23 22:37:22 +0800

    fix: 修复代码生成路径问题

--- code-generator-basic/src/main/java/com/fantasy/generator/MainGenerator.java
+++ code-generator-basic/src/main/java/com/fantasy/generator/MainGenerator.java
@@ -23,12 +23,12 @@ public class MainGenerator {
         // 整个项目的根路径
         File parentFile = new File(projectPath).getParentFile();
         // 输入路径
-        String inputPath = new File(parentFile, "code-generator/code-generator-demo-projects/acm-template").getAbsolutePath();
+        String inputPath = new File(parentFile, "code-generator-demo-projects/acm-template").getAbsolutePath();
         String outputPath = projectPath;
         // 生成静态文件
         StaticGenerator.copyFilesByRecursive(inputPath, outputPath);
         // 生成动态文件
-        String inputDynamicFilePath = projectPath + File.separator + "code-generator-basic" + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String inputDynamicFilePath = projectPath + File.separator + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
         String outputDynamicFilePath = outputPath + File.separator + "acm-template/src/com/fantasy/acm/MainTemplate.java";
         DynamicGenerator.doGenerate(inputDynamicFilePath, outputDynamicFilePath, model);
     }
