f83b3b1afa500f5ffd9bd050d2bf369f6d870bfa
Fantasy <1362724990@qq.com>
2026-01-05 22:44:18 +0800

    feat: 云平台开发 - 后端

---
+++ code-generator-web-backend/src/main/java/com/fantasy/web/mapper/GeneratorMapper.java
@@ -0,0 +1,18 @@
+package com.fantasy.web.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.fantasy.web.model.entity.Generator;
+
+/**
+ * @author Fantasy
+ * @description 针对表【generator(代码生成器)】的数据库操作Mapper
+ * @createDate 2026-01-05 21:05:18
+ * @Entity com.fantasy.web.model.entity.Generator
+ */
+public interface GeneratorMapper extends BaseMapper<Generator> {
+
+}
+
+
+
+
