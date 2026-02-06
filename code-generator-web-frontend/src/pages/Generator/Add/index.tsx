fec6183bea6f76c50192c586d2e9c1b318132f3d
Fantasy <1362724990@qq.com>
2026-02-06 23:53:58 +0800

    feat: 在线使用生成器

--- code-generator-web-frontend/src/pages/Generator/Add/index.tsx
+++ code-generator-web-frontend/src/pages/Generator/Add/index.tsx
@@ -1,6 +1,7 @@
 import FileUploader from '@/components/FileUploader';
 import PictureUploader from '@/components/PictureUploader';
 import { COS_HOST } from '@/constants';
+import ModelConfigForm from '@/pages/Generator/Add/components/ModelConfigForm';
 import {
   addGeneratorUsingPost,
   editGeneratorUsingPost,
@@ -18,6 +19,7 @@ import {
 import { ProFormItem } from '@ant-design/pro-form';
 import { history } from '@umijs/max';
 import { message, UploadFile } from 'antd';
+import { values } from 'lodash';
 import React, { useEffect, useRef, useState } from 'react';
 
 /**
@@ -165,8 +167,15 @@ const GeneratorAddPage: React.FC = () => {
           <StepsForm.StepForm name="fileConfig" title="文件配置">
             {/*todo 待补充*/}
           </StepsForm.StepForm>
-          <StepsForm.StepForm name="modelConfig" title="模型配置">
-            {/*todo 待补充*/}
+          <StepsForm.StepForm
+            name="modelConfig"
+            title="模型配置"
+            onFinish={async () => {
+              console.log(values);
+              return true;
+            }}
+          >
+            <ModelConfigForm formRef={formRef} oldData={oldData} />
           </StepsForm.StepForm>
           <StepsForm.StepForm name="dist" title="生成器文件">
             <ProFormItem label="产物包" name="distPath">
